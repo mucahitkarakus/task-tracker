e3ad856aeac0e1ed99443172e62d83e1156165e6
mucahitkarakus <mocococo.80@gmail.com>
2022-10-19 17:06:31 +0300

    left local storage

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,18 +2,17 @@ import React, {useState} from "react";
 import {Header, AddTask, Task} from ".";
 
 const Layout = () => {
-  const [isActive, setisActive] = useState(false);
-
+  const [isActive, setIsActive] = useState(false);
   const [data, setData] = useState([
     {
       id: new Date().getMilliseconds(),
-      title: `Alpha Team`,
-      date: `Dec 12 12.00PM`,
+      title: "The Alpha Team",
+      date: "Dec 12 12.00PM",
       complated: false,
     },
   ]);
 
-  //Func
+  // Func
   const handleSubmit = (e) => {
     e.preventDefault();
     const newTodo = {
@@ -24,17 +23,28 @@ const Layout = () => {
     };
     setData([...data, newTodo]);
   };
-  //Delete Func
+
+  // Delete Func
   const handleRemove = (id) => {
     setData(data.filter((todo) => todo.id !== id));
   };
 
-  //Func section is done
+  // Complated Func
+  const handleComplated = (id) => {
+    const updatedData = data.map((todo) => {
+      if (todo.id === id) {
+        return {...todo, complated: !todo.complated};
+      }
+      return todo;
+    });
+    setData(updatedData);
+  };
+  // Func Section is Done
   return (
     <div className="mt-[2.5rem] w-[40rem] bg-pink-500 pb-10">
-      <Header active={isActive} setActive={setisActive} />
+      <Header active={isActive} setActive={setIsActive} />
       {isActive && (
-        <div className="w-[30rem] mx-auto d">
+        <div className="w-[30rem] mx-auto">
           <AddTask handleSubmit={handleSubmit} />
         </div>
       )}
@@ -42,7 +52,11 @@ const Layout = () => {
         <div className="max-h-[25rem] overflow-y-auto">
           {data.map((item, idx) => (
             <div key={idx}>
-              <Task data={item} handleRemove={handleRemove} />
+              <Task
+                data={item}
+                handleRemove={handleRemove}
+                handleComplated={handleComplated}
+              />
             </div>
           ))}
         </div>
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import {AiOutlineClose} from "react-icons/ai";
 
-const Task = ({data, handleRemove}) => {
+const Task = ({data, handleRemove, handleComplated}) => {
   return (
     <div className="w-[30rem] mx-auto py-3">
       <div className={`bg-white p-4 flex justify-between items-center`}>
-        <div className="w-full cursor-pointer">
+        <div
+          className="w-full cursor-pointer"
+          onClick={() => handleComplated(data.id)}
+        >
           <p className={`font-bold text-xl`}>{data.title}</p>
-          <p className={``}>{data.date}</p>
+          <p className={`${data.complated && "line-through"}`}>{data.date}</p>
         </div>
         <div
-          onClick={() => handleRemove(data.id)}
           className="text-xl text-red-800 cursor-pointer p-2 bg-slate-200 rounded-full"
+          onClick={() => handleRemove(data.id)}
         >
           <AiOutlineClose />
         </div>
