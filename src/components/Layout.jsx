c8888fc546c66f13c4a622adff17c816626e0894
mucahitkarakus <mocococo.80@gmail.com>
2022-10-19 16:35:43 +0300

    ternary if done

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,18 +4,24 @@ import {Header, AddTask, Task} from ".";
 const Layout = () => {
   const [isActive, setisActive] = useState(false);
 
-  const Data = [
+  const [data, setData] = useState([
     {
       id: new Date().getMilliseconds(),
       title: `Alpha Team`,
       date: `Dec 12 12.00PM`,
       complated: false,
     },
-  ];
+  ]);
 
   //Func
   const handleSubmit = (e) => {
     e.preventDefault();
+    const newTodo = {
+      id: new Date().getMilliseconds(),
+      title: e.target.taskName.value,
+      date: e.target.selectt.value,
+      complated: false,
+    };
   };
 
   //Func section is done
@@ -27,11 +33,17 @@ const Layout = () => {
           <AddTask handleSubmit={handleSubmit} />
         </div>
       )}
-      {Data.map((item, idx) => (
-        <div key={idx}>
-          <Task data={item} />
+      {data.length !== 0 ? (
+        <div>
+          {data.map((item, idx) => (
+            <div>
+              <Task data={item} />
+            </div>
+          ))}
         </div>
-      ))}
+      ) : (
+        <p className="text-center text-xl font-semibold">No Task Yet</p>
+      )}
     </div>
   );
 };
