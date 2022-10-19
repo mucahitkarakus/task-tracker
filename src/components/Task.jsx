44b2556392bde7a6e82da9e6be17bd1ea711c232
mucahitkarakus <mocococo.80@gmail.com>
2022-10-19 16:09:20 +0300

    task styling

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Task = () => {
-  return <div>Task</div>;
+  return (
+    <div className="w-[30rem] mx-auto py-3">
+      <div>as</div>
+    </div>
+  );
 };
 
 export default Task;
